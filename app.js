87aed35809e7426e734d931b7e2acb2caa7296c6
rapsori <rapsori@gmail.com>
2022-10-04 13:22:04 +0900

    functions part two

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-function sayHello() {
-  console.log("Hello my name is C");
-}
+// function sayHello(nameOfPerson, age) {
+//   console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
 
-sayHello("nico");
-sayHello("dal");
-sayHello("lynn");
+// sayHello("nico", 10);
+// sayHello("dal", 23);
+// sayHello("lynn", 21);
+
+// function plus(firstNumber, secondNumber) {
+//   console.log(firstNumber + secondNumber);
+// }
+
+// function divide(a, b) {
+//   console.log(a / b);
+// }
+
+// plus(60, 8);
+// divide(98, 20);
+
+const player = {
+  name: "nico",
+  sayHello: function (otherPersonsName) {
+    console.log("Hello " + otherPersonsName + " nice to meet you!");
+  },
+};
+
+player.sayHello("lynn");
+player.sayHello("nico");
