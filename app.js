04c310361d8e6fb02e115606378899e80f672e81
rapsori <rapsori@gmail.com>
2022-10-04 11:48:21 +0900

    objects

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("hi");
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+// update
+player.fat = false;
+console.log(player);
+player.points = player.points + 15;
+console.log(player.points);
+
+// add
+player.lastName = "potato";
+console.log(player);
