79df951382dd165c6eb620f0ea7b9bd8133c62c4
rapsori <rapsori@gmail.com>
2022-10-04 13:37:26 +0900

    returns

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const calculator = {
   plus: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
   },
   times: function (a, b) {
-    console.log(a * b);
+    return a * b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
   power: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
 };
 
-calculator.plus(2, 3);
-calculator.times(2, 3);
-calculator.minus(2, 3);
-calculator.divide(2, 3);
-calculator.power(2, 3);
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
