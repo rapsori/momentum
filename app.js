b5e347a3528791b2b959532a178e00fb75d39b77
rapsori <rapsori@gmail.com>
2022-10-04 13:49:00 +0900

    conditionals

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
+console.log(age);
